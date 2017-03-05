f0d8b081a9ec8748e9a07e8828406da200fe1287
Liu Yuan Xin <Yuanxin1007@gmail.com>
2017-03-05 21:14:21 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,14 +3,15 @@ class Job < ApplicationRecord
      validates :wage_upper_bound, presence: true
      validates :wage_lower_bound, presence: true
      validates :wage_lower_bound, numericality: { greater_than: 0}
+     has_many :resumes
      def publish!
   self.is_hidden = false
   self.save
-end
+  end
 scope :published, -> { where(is_hidden: false) }
 scope :recent, -> { order('created_at DESC') }
-def hide!
+  def hide!
   self.is_hidden = true
   self.save
-end
+  end
 end
